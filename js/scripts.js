9d4325aa6728aab25589124aaa09dd1046d5a3c2
Aliweb23 <haschimi7219@gmail.com>
2024-08-14 10:37:02 +0200

    index & css style & script.js modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-    // Toggle the hamburger menu
-    $('#menu').click(function() {
-        $(this).toggleClass('fas fa-bars fa-times'); // Wechsel zwischen fa-bars und fa-times
-        $('header').toggleClass('toggle'); // Öffnen und Schließen der Navbar
+    $(document).ready(function() {
+        // Toggle the hamburger menu
+        $('#menu').click(function() {
+            $(this).toggleClass('fa-bars fa-times'); // Wechsel zwischen fa-bars und fa-times
+            $('#header').toggleClass('toggle'); // Öffnen und Schließen der Navbar
+        });
     });
 
     // Entfernt die Toggle-Klasse beim Scrollen oder Laden der Seite
     $(window).on('scroll load', function() {
         $('#menu').removeClass('fa-times').addClass('fa-bars'); // Setzt das Hamburger-Icon auf fa-bars zurück
-        $('header').removeClass('toggle'); // Schließt die Navbar beim Scrollen
+        $('#header').removeClass('toggle'); // Schließt die Navbar beim Scrollen
         if ($(window).scrollTop() > 0) {
             $('.top').show(); // Zeigt das "top"-Element, wenn gescrollt wird
         } else {
