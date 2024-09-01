6e5450a0239d3ec7a0e3a08b4b695dbed7a33598
Aliweb23 <haschimi7219@gmail.com>
2024-09-01 23:48:41 +0200

    hallo

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
     $(document).ready(function() {
         // Toggle the hamburger menu
         $('#menu').click(function() {
-            $(this).toggleClass('fa-bars fa-times'); // Wechsel zwischen fa-bars und fa-times
-            $('#header').toggleClass('toggle'); // Öffnen und Schließen der Navbar
+            $(this).toggleClass('fa-times'); 
+            $('#header').toggleClass('toggle'); 
         });
     });
 
     // Entfernt die Toggle-Klasse beim Scrollen oder Laden der Seite
+$(window).on('scroll load', function() {
+    $('#menu').removeClass('fa-times');
+    $('#header').removeClass('toggle');
+});
+    // Entfernt die Toggle-Klasse beim Scrollen oder Laden der Seite
     $(window).on('scroll load', function() {
-        $('#menu').removeClass('fa-times').addClass('fa-bars'); // Setzt das Hamburger-Icon auf fa-bars zurück
-        $('#header').removeClass('toggle'); // Schließt die Navbar beim Scrollen
+        $('#menu').removeClass('fa-times'); 
+        $('header').removeClass('toggle'); 
         if ($(window).scrollTop() > 0) {
-            $('.top').show(); // Zeigt das "top"-Element, wenn gescrollt wird
+            $('.top').show();
         } else {
-            $('.top').hide(); // Versteckt das "top"-Element, wenn nicht gescrollt wird
+            $('.top').hide(); 
         }
     });
 
@@ -27,6 +32,6 @@
 
     // Öffnet ein neues Fenster für den CV-Download-Link
     $('.btn[href*="lebenslauf-ali-haschimi"]').on('click', function(e) {
-        e.preventDefault(); // Verhindert das Standardverhalten des Links
-        window.open($(this).attr('href'), '_blank'); // Öffnet den Link in einem neuen Tab
+        e.preventDefault(); 
+        window.open($(this).attr('href'), '_blank'); 
     });
